1007dee0aa95fa8177edc94bf1e45717f218c71a
Tomasz Nazar (aspectized) <tomasz.nazar@gmail.com>
2013-01-11 06:30:37 -0600

    file log added

--- run.sh
+++ run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
+date >> log
 
 if [ -e src/configuration.rb ];
 then
   echo "Configuration found, running notifier..."
-  ruby src/notifier.rb
+  ruby src/notifier.rb | tee -a log
 else
   echo "No configuration found! adjust & cp src/configuration.rb.example -> src/configuration.rb"
 fi
